c9c39aa6ac36e522cb4dfd42cc9ab70c46bd3edf
meoldae <wnsdud12365@naver.com>
2024-02-08 18:11:48 +0900

    BooleanBuilder를 통한 동적 쿼리 테스트 추가

--- querydsl/src/test/java/study/querydsl/MemberJPARepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/MemberJPARepositoryTest.java
@@ -1,11 +1,17 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+import study.querydsl.repository.MemberJPARepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,9 +28,30 @@ class MemberJPARepositoryTest {
     @Autowired
     MemberJPARepository memberJPARepository;
 
+    @BeforeEach
+    public void setTestCase(){
+        Team teamA = new Team("TeamA");
+        Team teamB = new Team("TeamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("Member1", 10, teamA);
+        Member member2 = new Member("Member2", 20, teamA);
+        Member member3 = new Member("Member3", 30, teamB);
+        Member member4 = new Member("Member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+    }
+
     @Test
     public void basicTest() {
-        String username = "Member1";
+        String username = "Member5";
         int age = 10;
 
         Member member1 = new Member(username,  age);
@@ -38,7 +65,7 @@ class MemberJPARepositoryTest {
         );
 
         List<Member> result = memberJPARepository.findAll();
-        assertThat(result).containsExactly(member1);
+        assertThat(result).contains(member1);
 
         List<Member> result2 = memberJPARepository.findByUsername(username);
         assertThat(result2).containsExactly(member1);
@@ -46,7 +73,7 @@ class MemberJPARepositoryTest {
 
     @Test
     public void basicQueryDslTest() {
-        String username = "Member1";
+        String username = "Member5";
         int age = 10;
 
         Member member1 = new Member(username,  age);
@@ -60,9 +87,27 @@ class MemberJPARepositoryTest {
         );
 
         List<Member> result = memberJPARepository.findAllQueryDsl();
-        assertThat(result).containsExactly(member1);
+        assertThat(result).contains(member1);
 
         List<Member> result2 = memberJPARepository.findByUsernameQueryDsl(username);
         assertThat(result2).containsExactly(member1);
+    }
+
+    @Test
+    public void searchTest() {
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("TeamB");
+
+        List<MemberTeamDto> result = memberJPARepository.searchMemberTeamDtoByCondition(condition);
+        assertThat(result).extracting("username").containsExactly("Member4");
+
+        MemberSearchCondition condition2 = new MemberSearchCondition();
+        condition.setTeamName("TeamB");
+
+        List<MemberTeamDto> result2 = memberJPARepository.searchMemberTeamDtoByCondition(condition2);
+        assertThat(result2).extracting("username").contains("Member3", "Member4");
+
     }
 }
