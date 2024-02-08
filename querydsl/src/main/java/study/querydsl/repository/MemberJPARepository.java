86d470a32ddfbdb8db5322eb0b7f2c9faadc30db
meoldae <wnsdud12365@naver.com>
2024-02-08 18:11:02 +0900

    파일 위치 변경

--- querydsl/src/main/java/study/querydsl/repository/MemberJPARepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberJPARepository.java
@@ -1,15 +1,22 @@
-package study.querydsl;
+package study.querydsl.repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QTeam;
 
 import java.util.List;
 import java.util.Optional;
 
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
 @Repository
 @RequiredArgsConstructor
@@ -58,6 +65,38 @@ public class MemberJPARepository {
         return jpaQueryFactory
                 .selectFrom(member)
                 .where(member.username.eq(username))
+                .fetch();
+    }
+
+    public List<MemberTeamDto> searchMemberTeamDtoByCondition(MemberSearchCondition memberSearchCondition){
+        BooleanBuilder builder = new BooleanBuilder();
+        if (StringUtils.hasText(memberSearchCondition.getUsername())) {
+            builder.and(member.username.eq(memberSearchCondition.getUsername()));
+        }
+
+        if (StringUtils.hasText(memberSearchCondition.getTeamName())) {
+            builder.and(team.teamName.eq(memberSearchCondition.getTeamName()));
+        }
+
+        if (memberSearchCondition.getAgeGoe() != null) {
+            builder.and(member.age.goe(memberSearchCondition.getAgeGoe()));
+        }
+
+        if (memberSearchCondition.getAgeLoe() != null) {
+            builder.and(member.age.loe(memberSearchCondition.getAgeLoe()));
+        }
+
+        return jpaQueryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        member.team.id.as("teamId"),
+                        member.team.teamName.as("teamName")
+                        ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(builder)
                 .fetch();
     }
 }
