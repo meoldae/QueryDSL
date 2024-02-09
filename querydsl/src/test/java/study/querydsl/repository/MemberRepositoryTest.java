b429fd7c82a1eb3bdbacb9a478bb357576700d12
meoldae <wnsdud12365@naver.com>
2024-02-09 16:34:34 +0900

    동적쿼리 테스트 구문 추가

--- querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,11 +1,15 @@
 package study.querydsl.repository;
 
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +26,27 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @BeforeEach
+    public void setTestCase(){
+        Team teamA = new Team("TeamA");
+        Team teamB = new Team("TeamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("Member1", 10, teamA);
+        Member member2 = new Member("Member2", 20, teamA);
+        Member member3 = new Member("Member3", 30, teamB);
+        Member member4 = new Member("Member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+    }
 
     @Test
     public void basicTest() {
@@ -43,6 +68,24 @@ class MemberRepositoryTest {
 
         List<Member> result2 = memberRepository.findByUsername(username);
         assertThat(result2).containsExactly(member1);
+    }
+
+    @Test
+    public void searchTest() {
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("TeamB");
+
+        List<MemberTeamDto> result = memberRepository.search(condition);
+        assertThat(result).extracting("username").containsExactly("Member4");
+
+        MemberSearchCondition condition2 = new MemberSearchCondition();
+        condition.setTeamName("TeamB");
+
+        List<MemberTeamDto> result2 = memberRepository.search(condition2);
+        assertThat(result2).extracting("username").contains("Member3", "Member4");
+
     }
 
 }
