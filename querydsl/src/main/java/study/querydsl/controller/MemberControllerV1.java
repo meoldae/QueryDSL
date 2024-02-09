e0c55e4f35fa640afbc456831ae9444afad64120
meoldae <wnsdud12365@naver.com>
2024-02-09 17:34:48 +0900

    Paging API 추가

--- querydsl/src/main/java/study/querydsl/controller/MemberControllerV1.java
+++ querydsl/src/main/java/study/querydsl/controller/MemberControllerV1.java
@@ -2,6 +2,8 @@ package study.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,21 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.repository.MemberJPARepository;
+import study.querydsl.repository.MemberRepository;
 
 import java.util.List;
 
 @Log4j2
 @RestController
-@RequestMapping("/v1")
 @RequiredArgsConstructor
 public class MemberControllerV1 {
 
     private final MemberJPARepository memberJPARepository;
+    private final MemberRepository memberRepository;
 
-    @GetMapping("/members")
+    @GetMapping("/v1/members")
     public ResponseEntity<List<MemberTeamDto>> memberSearchV1(MemberSearchCondition condition) {
-        log.info("[[V1 Members Called]]");
         List<MemberTeamDto> members = memberJPARepository.searchMemberTeamDtoByCondition(condition);
+        return new ResponseEntity<>(members, HttpStatus.OK);
+    }
+
+    @GetMapping("/v2/members")
+    public ResponseEntity<Page<MemberTeamDto>> memberSearchV2(MemberSearchCondition condition, Pageable pageable) {
+        Page<MemberTeamDto> members = memberRepository.searchPageComplex(condition, pageable);
         return new ResponseEntity<>(members, HttpStatus.OK);
     }
 }
